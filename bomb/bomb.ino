81d38c6f9df7063428cf089dcdee61f8b2377160
AndresArangoVelez <andrsarngvl@gmailcom>
2022-02-22 17:11:58 -0500

    change SSD to 128_32

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -7,8 +7,8 @@
 #define ARM_BTN 33
 
 // Selecciona uno según tu display.
-//SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
-SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48); 
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
+//SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48); 
 
 void setup() {
 
