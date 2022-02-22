734b94a8c3c629a1bd4ca5ec9f9a16dfe4f8a827
AndresArangoVelez <andrsarngvl@gmailcom>
2022-02-22 18:08:50 -0500

    add BombStates::CONFIG(delays)

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -1,10 +1,10 @@
 #include "SSD1306Wire.h"
 
-#define BOMB_OUT 25
-#define LED_COUNT 26
-#define UP_BTN 13
-#define DOWN_BTN 32
-#define ARM_BTN 33
+#define LED1BOMB_OUT 25
+#define LED2_COUNT 26
+#define UP_BTN1 13
+#define DOWN_BTN3 32
+#define ARM_BTN2 33
 
 // Selecciona uno según tu display.
 SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
@@ -20,20 +20,24 @@ void task() {
   enum class BombStates {INIT, CONFIG, ARMED};
   static BombStates bombState =  BombStates::INIT;
   static uint8_t counter;
+  static int x;
+  static int y;
 
 
 
   switch (bombState) {
     case BombStates::INIT: {
         counter = 20;
+        x = 0;
+        y = 0;
+        pinMode(LED1BOMB_OUT, OUTPUT);
+        pinMode(LED2_COUNT, OUTPUT);
+        pinMode(UP_BTN1, INPUT_PULLUP);
+        pinMode(DOWN_BTN3, INPUT_PULLUP);
+        pinMode(ARM_BTN2, INPUT_PULLUP);
 
-        /* Inicializar los puertos:
-          #define BOMB_OUT 25
-          #define LED_COUNT 26
-          #define UP_BTN 13
-          #define DOWN_BTN 32
-          #define ARM_BTN 33
-        */
+
+
         display.init();
         display.setContrast(255);
         display.clear();
@@ -49,10 +53,41 @@ void task() {
         break;
       }
     case BombStates::CONFIG: {
+
+        if ((digitalRead(UP_BTN1) == LOW) && (counter < 60) && (x == 0)) {
+          counter++ ;
+          display.clear();
+          display.drawString(0, 5, String(counter));
+          display.display();
+          x = 1;
+          delay(100);
+        }
+        else if (digitalRead(UP_BTN1) == HIGH ) {
+          x = 0;
+          delay(100);
+        }
+        if ((digitalRead(DOWN_BTN3) == LOW) && (counter > 0)&& (y == 0)) {
+          counter-- ;
+          display.clear();
+          display.drawString(0, 5, String(counter));
+          display.display();
+          delay(100);
+        }
+        else if (digitalRead(DOWN_BTN3) == HIGH ) {
+          y = 0;
+          delay(100);
+        }
+
+if (digitalRead(ARM_BTN2) == LOW ){
+  bombState = BombStates::ARMED;
+  }
+
         break;
       }
 
     case BombStates::ARMED: {
+
+      digitalWrite(LED2_COUNT,HIGH);
         break;
       }
 
